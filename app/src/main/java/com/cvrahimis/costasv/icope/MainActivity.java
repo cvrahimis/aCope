594564b9b50892f6ac42df208a04741bbc37e22e
Costas Vrahimis <cvrahimis.11@gmail.com>
2015-02-18 11:28:15 -0500

    Made Thermometer Nigga but WIP

--- app/src/main/java/com/cvrahimis/costasv/icope/MainActivity.java
+++ app/src/main/java/com/cvrahimis/costasv/icope/MainActivity.java
@@ -2,13 +2,21 @@ package com.cvrahimis.costasv.icope;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -21,12 +29,47 @@ import java.util.Date;
 
 public class MainActivity extends ActionBarActivity {
 
+    int screenWidth;
+    int screenHeight;
+
+    private GestureDetector gestureDetector;
+    private AbsoluteLayout.LayoutParams thermometerLayoutParams;
+    private AbsoluteLayout.LayoutParams mesurmentViewLayoutParams;
+    private ImageView mesurmentView;
+    private ImageView thermometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gestureDetector = new GestureDetector(getApplicationContext(), new SwipeGestureDetector());
+
         final RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.ratingScreenMainLayout);
+        final AbsoluteLayout absLayout = (AbsoluteLayout) mainLayout.findViewById(R.id.urgeFrameView);
+
+        mesurmentView = (ImageView) absLayout.findViewById(R.id.mesurmentView);
+        mesurmentView.bringToFront();
+
+        thermometer = (ImageView) absLayout.findViewById(R.id.thermometer);
+        thermometerLayoutParams = (AbsoluteLayout.LayoutParams)thermometer.getLayoutParams();
+
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        //Toast.makeText(getApplicationContext(), String.valueOf(thermometerLayoutParams.height), Toast.LENGTH_SHORT).show();
+
+        mesurmentViewLayoutParams = (AbsoluteLayout.LayoutParams)mesurmentView.getLayoutParams();
+        mesurmentViewLayoutParams.x = (int)Math.floor(screenWidth * .14);
+        mesurmentViewLayoutParams.y = ((int)Math.floor(screenHeight * .01)) + 2;
+        mesurmentViewLayoutParams.height = thermometerLayoutParams.height * -32;
+
+        //mesurmentView.setLayoutParams(absParams);
 
         Drawable d;
 
@@ -50,13 +93,6 @@ public class MainActivity extends ActionBarActivity {
             d = getResources().getDrawable(R.drawable.morning);
             mainLayout.setBackground(d);
         }
-
-        ImageView thermometer = (ImageView) mainLayout.findViewById(R.id.thermometer);
-        ImageView tempDrag = (ImageView) mainLayout.findViewById(R.id.temperatureDrag);
-        ViewGroup.LayoutParams thermometerLayoutParams = thermometer.getLayoutParams();
-        ViewGroup.LayoutParams tempDragLayoutParams = tempDrag.getLayoutParams();
-        thermometerLayoutParams.height = (int)Math.floor(tempDragLayoutParams.height * .8);
-        thermometerLayoutParams.width = (int)Math.floor(tempDragLayoutParams.width * .05);
     }
 
     @Override
@@ -105,6 +141,68 @@ public class MainActivity extends ActionBarActivity {
                 Intent intent = new Intent(this, MenuActivity.class);
                 startActivity(intent);
             }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void onLeftSwipe(double d) {
+        mesurmentViewLayoutParams.width = mesurmentViewLayoutParams.width - (int)Math.floor(d);
+        mesurmentView.setLayoutParams(mesurmentViewLayoutParams);
+
+    }
+
+    private void onRightSwipe(double d) {
+        mesurmentViewLayoutParams.width = mesurmentViewLayoutParams.width + (int)Math.floor(d);
+        mesurmentView.setLayoutParams(mesurmentViewLayoutParams);
+    }
+
+
+    // Private class for gestures
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private double SWIPE_MIN_DISTANCE;
+        private double SWIPE_MAX_OFF_PATH;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 100;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            //SWIPE_MIN_DISTANCE = screenWidth * .25;
+            SWIPE_MAX_OFF_PATH = screenHeight * .2;
+            try {
+                float heightDiff = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                /*if (heightDiff > SWIPE_MAX_OFF_PATH)
+                {
+                    Toast.makeText(getApplicationContext(),"height: " + SWIPE_MAX_OFF_PATH, Toast.LENGTH_LONG).show();
+                    return false;
+                }*/
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(getApplicationContext(),"diff: " + diff, Toast.LENGTH_LONG).show();
+                    MainActivity.this.onLeftSwipe(diff);
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(getApplicationContext(),"diff: " + diff, Toast.LENGTH_LONG).show();
+                    MainActivity.this.onRightSwipe(-diff);
+                }
+
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
         }
     }
 }
